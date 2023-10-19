78823fb031893f4630923dae84b185867d07e456
harshil sharma <119157246+Harshil2502@users.noreply.github.com>
2023-10-19 17:06:27 +0530

    Update Ticker.tsx

--- components/Ticker.tsx
+++ components/Ticker.tsx
@@ -8,7 +8,7 @@ export default function Ticker() {
   
   return (
     <div className="mt-5 grid grid-cols-1 md:grid-cols-3 gap-6">
-     {cryptocurrencies?.map((crypto)=>(
+     {cryptocurrencies?.map((crypto:any)=>(
       <Crypto key={crypto.id} crypto={crypto}/>
      ))}
     </div>
